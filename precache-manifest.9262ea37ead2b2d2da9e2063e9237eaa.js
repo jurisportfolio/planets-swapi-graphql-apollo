83c81ed0565032cb631194639f5d1b5254dae137
jurkotroll <jurko.bossetroll@gmail.com>
2019-05-03 17:21:06 +0200

    Updates

---
+++ precache-manifest.9262ea37ead2b2d2da9e2063e9237eaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a55aef5d574e3fffa4d6a46f86038b",
+    "url": "/planets-swapi-graphql-apollo/index.html"
+  },
+  {
+    "revision": "ce421d87a54134f4307b",
+    "url": "/planets-swapi-graphql-apollo/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "dfafce5bc0256766a6ac",
+    "url": "/planets-swapi-graphql-apollo/static/js/2.62383f9c.chunk.js"
+  },
+  {
+    "revision": "ce421d87a54134f4307b",
+    "url": "/planets-swapi-graphql-apollo/static/js/main.d6a4012c.chunk.js"
+  },
+  {
+    "revision": "97472796819d7cafb1b6",
+    "url": "/planets-swapi-graphql-apollo/static/js/runtime~main.929029bd.js"
+  }
+]);
