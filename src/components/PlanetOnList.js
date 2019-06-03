3519e02f02e9753715b6ace88aed76e4f4c5929a
jurkotroll <jurko.bossetroll@gmail.com>
2019-06-03 11:30:56 +0200

    refactoring PlanetOnListComponent component

--- src/components/PlanetOnList.js
+++ src/components/PlanetOnList.js
@@ -31,6 +31,55 @@ const StyledPlanetOnList = styled.div`
   }
 `;
 
+const somePlanetInfo = (info = " No information", unit = "") => {
+  return ` ${info} ${unit}`.trim()
+}
+
+
+const BottomPartOfPlanetOnListComponent = ({ id }) =>
+
+  <Query query={PLANET_ALL_INFO} variables={{ planetID: id }}>
+    {({ loading, error, data }) => {
+      if (loading) return <h5>LOADING...</h5>;
+      if (error) return <h5>NO INFORMATION</h5>;
+      const { gravity, rotationPeriod, orbitalPeriod, climates, terrains, filmConnection, residentConnection } = data.planet;
+      return (
+        <div>
+          <h5>Gravity: {somePlanetInfo(gravity)}</h5>
+          <h5>Rotation Period: {somePlanetInfo(rotationPeriod)}</h5>
+          <h5>Orbital Period: {somePlanetInfo(orbitalPeriod)}</h5>
+          <h5>Climates: {somePlanetInfo(climates.map((climate) => ` ${climate}`))}</h5>
+          <h5>Terrains: {somePlanetInfo(terrains.map((terrain) => ` ${terrain}`))}</h5>
+          {filmConnection.films.length > 0 ?
+            <div>
+              <hr /><h5>Films where you could see this planet:</h5>
+              <ul>{filmConnection.films.map((film) => <li key={film.id}><h5>{film.title}</h5></li>)}</ul>
+            </div> : null}
+          {residentConnection.residents.length > 0 ?
+            <div>
+              <hr /><h5>Persons which connect to this planet:</h5>
+              <ul>{residentConnection.residents.map((resident) => <li key={resident.id}><h5>{resident.name}</h5></li>)}</ul>
+            </div> : null}
+        </div>
+      )
+    }
+    }
+  </Query>
+
+const TopPartOfPlanetOnListComponent = ({ aboutPlanet }) => {
+  const { name, diameter, population, surfaceWater } = aboutPlanet;
+  return (
+    <React.Fragment>
+      <div>
+        <h5>Planet</h5>
+        <h3>{somePlanetInfo(name)}</h3>
+      </div>
+      <h5>Diameter: {somePlanetInfo(diameter, "km")}</h5>
+      <h5>Population: {somePlanetInfo(population, "persons")}</h5>
+      <h5>Water surface: {somePlanetInfo(surfaceWater, "%")}</h5>
+    </React.Fragment>)
+}
+
 class PlanetOnListComponent extends React.Component {
   state = {
     isPlanetFullInfoOpen: false
@@ -40,52 +89,14 @@ class PlanetOnListComponent extends React.Component {
     this.setState({ isPlanetFullInfoOpen: !this.state.isPlanetFullInfoOpen });
   }
 
-  somePlanetInfo = (info = " No information", unit = "") => {
-    return ` ${info} ${unit}`.trim()
-  }
-
   render() {
-    const { id, name, diameter, population, surfaceWater } = this.props.aboutPlanet;
-    return (
-      <StyledPlanetOnList onClick={this.handleOnClick} isPlanetFullInfoOpen={this.state.isPlanetFullInfoOpen}>
-        <div>
-          <h5>Planet</h5>
-          <h3>{this.somePlanetInfo(name)}</h3>
-        </div>
-        <h5>Diameter: {this.somePlanetInfo(diameter, "km")}</h5>
-        <h5>Population: {this.somePlanetInfo(population, "persons")}</h5>
-        <h5>Water surface: {this.somePlanetInfo(surfaceWater, "%")}</h5>
-        {this.state.isPlanetFullInfoOpen ?
-          <Query query={PLANET_ALL_INFO} variables={{ planetID: id }}>
-            {
-              ({ loading, error, data }) => {
-                if (loading) return <h5>LOADING...</h5>;
-                if (error) return <h5>NO INFORMATION</h5>;
-                const { gravity, rotationPeriod, orbitalPeriod, climates, terrains, filmConnection, residentConnection } = data.planet;
-                return (
-                  <div>
-                    <h5>Gravity: {this.somePlanetInfo(gravity)}</h5>
-                    <h5>Rotation Period: {this.somePlanetInfo(rotationPeriod)}</h5>
-                    <h5>Orbital Period: {this.somePlanetInfo(orbitalPeriod)}</h5>
-                    <h5>Climates: {this.somePlanetInfo(climates.map((climate) => ` ${climate}`))}</h5>
-                    <h5>Terrains: {this.somePlanetInfo(terrains.map((terrain) => ` ${terrain}`))}</h5>
-                    {filmConnection.films.length > 0 ?
-                      <div>
-                        <hr /><h5>Films where you could see this planet:</h5>
-                        <ul>{filmConnection.films.map((film) => <li key={film.id}><h5>{film.title}</h5></li>)}</ul>
-                      </div> : null}
-                    {residentConnection.residents.length > 0 ?
-                      <div>
-                        <hr /><h5>Persons which connect to this planet:</h5>
-                        <ul>{residentConnection.residents.map((resident) => <li key={resident.id}><h5>{resident.name}</h5></li>)}</ul>
-                      </div> : null}
-                  </div>
-                )
-              }
-            }
-          </Query>
+    const aboutPlanet = this.props.aboutPlanet;
 
-          : null}
+    return (
+      <StyledPlanetOnList onClick={this.handleOnClick} >
+        <TopPartOfPlanetOnListComponent aboutPlanet={aboutPlanet} />
+        {this.state.isPlanetFullInfoOpen ?
+          <BottomPartOfPlanetOnListComponent id={aboutPlanet.id} /> : null}
       </StyledPlanetOnList>
     );
   }
